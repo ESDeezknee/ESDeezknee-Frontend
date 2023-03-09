5a36818ff92a00e8bb86d4c194820789bca0df31
Wei Lun Teo <weilun.teo.2021@scis.smu.edu.sg>
2023-03-10 02:06:05 +0800

    challenge view

--- src/views/ChallengeView.vue
+++ src/views/ChallengeView.vue
@@ -5,26 +5,34 @@
         <div class="col-sm-6">
           <div class="mobile-phone bg-white">
             <!-- content of mobile phone -->
-            <div class="mobile-header">
+            <div class="mobile-header" style="display: flex;">
                 <div class="header-left">
                 <h2>MISSIONS</h2>
                 </div>
-            </div>
+                <div class="header-right">
+                    <img src="../assets/logo.svg" style="border-radius: 50%; width: 50px; height: 50px; border: 1px solid #ccc" alt="User Logo">
+                </div>
+           </div>
 
-            <button class="btn btn-secondary mt-3 mb-3 w-100" @click="viewActiveChallenges">View Active Challenges</button>
+            <button class="btn fw-semibold mb-2 w-100" 
+            style="color: black; border: 1px solid #ccc; font-size:small"
+            onmouseover="this.style.backgroundColor='#f1f5f9';" 
+            onmouseout="this.style.backgroundColor='#fff';" 
+            @click="viewActiveChallenges">View Active Challenges</button>
+
 
             <div class="mission-cards">
-                <b>FEATURED MISSIONS:</b>
+                <span class="fw-semibold" style="font-size:x-small; color:#6B7280">FEATURED MISSIONS:</span>
                 <div class="row flex-row">
                 <div class="col-12" v-for="mission in featuredMissions" :key="mission.id">
-                    <div class="card">
+                    <div class="card border border-0">
                     <div class="card-body bg-light">
-                        <h4 class="card-title">{{ mission.name }}</h4>
-                        <p class="card-text">{{ mission.description }}</p>
-                        <p class="card-text"><strong>Difficulty:</strong> {{ mission.difficulty }}</p>
-                        <p class="card-text"><strong>Duration:</strong> {{ mission.duration }}</p>
-                        <p class="card-text"><strong>Points:</strong> {{ mission.award_points }}</p>
-                        <button class="btn btn-success w-100" @click="joinChallenge(mission.id)">Join Challenge</button>
+                        <span class="card-title fw-bold" style="font-size:small">{{ mission.name }}</span><br>
+                        <span class="fw-light fst-italic" style="font-size:xx-small; color:#6B7280;">{{ mission.description }}</span><br>
+                        <span style="font-size:x-small" ><strong>Difficulty:</strong> {{ mission.difficulty }}</span><br>
+                        <span style="font-size:x-small"><strong>Duration:</strong> {{ mission.duration }}</span><br>
+                        <span style="font-size:x-small"><strong>Points:</strong> {{ mission.award_points }}</span>
+                        <button class="btn btn-success w-100 mt-2" style="font-size:small;" @click="joinChallenge(mission.id)">Join Challenge</button>
                     </div>
                     </div>
                 </div>
@@ -96,12 +104,23 @@ export default {
 
 <style>
 
+
+* {
+  font-family: 'Inter', sans-serif;
+}
+
+.mobile-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+}
+
 h2 {
     font-weight: bold;
     font-family: "Arial Rounded MT Bold", sans-serif;
 }
   
-
 .mobile-phone {
   border: 10px solid #000000;
   border-radius: 20px;
@@ -115,6 +134,12 @@ h2 {
 .card {
   margin-bottom: 20px;
 }
+
+.card-body:hover {
+  opacity: 0.9;
+  transition: opacity 0.2s ease;
+}
+
 
 </style>
   
