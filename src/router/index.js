f486115e82fa1e5aec67b37431aa7cedf6418169
ngkangting <benji.ngkt@gmail.com>
2023-03-29 12:47:21 +0800

    Edit

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,14 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import ChallengeView from '../views/ChallengeView.vue'
-import ActiveChallengesView from '../views/ActiveChallenges.vue' 
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import ChallengeView from "../views/ChallengeView.vue";
+import ActiveChallengesView from "../views/ActiveChallenges.vue";
 import RewardView from "../views/RewardView.vue";
-import RedemptionView from '../views/RedemptionView.vue'
-import CreateGroupView from '../views/CreateGroupView.vue'
-import InputGroupView from '../views/InputGroupView.vue'  
-import AllGroupsView from '../views/AllGroupsView.vue';
-import BroadcastView from '../views/BroadcastView.vue';
-import LoginView from '../views/LoginView.vue';
-import SignupView from '../views/SignupView.vue';
-
+import RedemptionView from "../views/RedemptionView.vue";
+import CreateGroupView from "../views/CreateGroupView.vue";
+import InputGroupView from "../views/InputGroupView.vue";
+import AllGroupsView from "../views/AllGroupsView.vue";
+import BroadcastView from "../views/BroadcastView.vue";
+import LoginView from "../views/LoginView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -44,42 +42,36 @@ const router = createRouter({
       component: RewardView,
     },
     {
-      path: '/redemption',
-      name: 'redemption',
-      component: RedemptionView
+      path: "/redemption",
+      name: "redemption",
+      component: RedemptionView,
     },
     {
-      path: '/create-group',
-      name: 'create-group',
-      component:CreateGroupView
+      path: "/create-group",
+      name: "create-group",
+      component: CreateGroupView,
     },
     {
-      path: '/input-group',
-      name: 'input-group',
-      component: InputGroupView
+      path: "/input-group",
+      name: "input-group",
+      component: InputGroupView,
     },
     {
-      path: '/all-groups',
-      name: 'all-groups',
-      component: AllGroupsView
-
-    },{
-      path: '/broadcast/:group_id',
-      name: 'broadcast',
-      component: BroadcastView
+      path: "/all-groups",
+      name: "all-groups",
+      component: AllGroupsView,
     },
     {
-      path: '/login',
-      name: 'login',
-      component: LoginView
+      path: "/broadcast/:group_id",
+      name: "broadcast",
+      component: BroadcastView,
     },
     {
-        path: '/signup',
-        name: 'signup',
-        component: SignupView
-    }
-
-  ]
-})
+      path: "/login",
+      name: "login",
+      component: LoginView,
+    },
+  ],
+});
 
 export default router;
