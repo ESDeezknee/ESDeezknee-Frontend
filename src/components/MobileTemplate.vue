f793f731451388c2f15a565ef74076235aadfc25
ngkangting <benji.ngkt@gmail.com>
2023-03-29 11:13:41 +0800

    Edit

--- src/components/MobileTemplate.vue
+++ src/components/MobileTemplate.vue
@@ -1,47 +1,45 @@
 <template>
-
-    <div class="container ">
-      <div class="row justify-content-center align-items-center" style="height: 100vh;">
-        <div class="col-sm-6">
-          <div class="mobile-phone bg-white">
-            <!-- content of mobile phone -->
-            <slot></slot>
-          </div>
+  <div class="container">
+    <div
+      class="row justify-content-center align-items-center"
+      style="height: 100vh"
+    >
+      <div class="">
+        <div class="mobile-phone bg-white">
+          <!-- content of mobile phone -->
+          <slot></slot>
         </div>
       </div>
     </div>
-
-  </template>
+  </div>
+</template>
 
 <script>
 import axios from "axios";
 
 export default {
-  name: 'MobileTemplate',
+  name: "MobileTemplate",
   // other options
-}
+};
 </script>
 
 <style>
-
-
 * {
-  font-family: 'Inter', sans-serif;
-
+  font-family: "Inter", sans-serif;
 }
 
 .mobile-header {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    margin-bottom: 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
 }
 
 h2 {
-    font-weight: bold;
-    font-family: "Arial Rounded MT Bold", sans-serif;
+  font-weight: bold;
+  font-family: "Arial Rounded MT Bold", sans-serif;
 }
-  
+
 .mobile-phone {
   border: 10px solid #000000;
   border-radius: 20px;
@@ -51,6 +49,4 @@ h2 {
   padding: 20px;
   overflow: scroll;
 }
-
 </style>
-  
